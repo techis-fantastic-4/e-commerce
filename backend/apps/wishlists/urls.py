d261a727b8f3c43d9089b86cc495a3a61a7af36f
Saadi <hardyhar06@gmail.com>
2021-07-14 15:43:45 -0500

    create api for wishlist.add and wishlist.delete

--- backend/apps/wishlists/urls.py
+++ backend/apps/wishlists/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.WishList.as_view(), name='wishlist'),
-    path('add/', views.WishlistAdd.as_view(), name='Wishlist_add'),
-    path('delete/<int:pk>/', views.WishlistDelete.as_view(), name='Wishlist_delete'),
+    path('add/', views.WishlistAdd.as_view(), name='wishlist_add'),
+    path('delete/<int:pk>/', views.WishlistDelete.as_view(), name='wishlist_delete'),
 ]

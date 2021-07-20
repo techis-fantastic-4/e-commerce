90718679c86a0c34892510b04284f48c7e0eea0e
yogendra bohara <sabiyogi19@gmail.com>
2021-07-19 21:07:48 -0700

    cart html created

--- frontend/src/containers/Cart.jsx
+++ frontend/src/containers/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import productCameraImage from '../assets/img/product-camera.jpg'
 
 //object
 // const cart = [
@@ -17,38 +18,38 @@ import React from 'react'
 const Cart = () => {
     return (
         <>
-            <h2>Shopping cart</h2>
-            <section>
-            <div className="col card mb-3" style="max-width: 540px;">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                    <img src="..." className="img-fluid rounded-start" alt="..."/>
-                    </div>
-                    <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col card mb-3" style="max-width: 540px;">
-                <div className="row g-0">
-                    <h2>Subtotal</h2>
-                    <p><span>(3)</span>ITEMS<span>$</span><p>649.97</p></p>
-                    <button>Proceed to checkout</button>
-                </div>
-            </div>
-
-            </section>
-        
+        <h2>SHOPPING CART</h2>
+        <section className="cart-component">
+            <div className="card">
+                <img className="imageview" 
+                src={productCameraImage}/>
+                <p>Sony PlayStation 4 Pro White Version</p>
+                <p><span>$</span>399.99</p>
+                
+                <div class="dropdown">
+  
+                <select name="cartitems" id="itemnumber">
+                    <option value="1">1</option> 
+                    <option value="2">2</option>      
+                    <option value="3">3</option>       
+                    <option value="4">4</option>
+                </select>
+            </div> |
+            <span><img src="https://img.icons8.com/material-two-tone/24/000000/trash.png"/></span>
             
-        </>
-        
+            </div>
+            <hr/>
             
-        
-    )
+        </section>
+        <div className="col card mb-3" >
+        <div className="row g-0">
+            <h2>Subtotal</h2>
+            <p><span>(3)</span>ITEMS<span>$</span><p>649.97</p></p>
+            <button>Proceed to checkout</button>
+        </div>
+    </div>
+    </>
+      )
 }
 
 export default Cart

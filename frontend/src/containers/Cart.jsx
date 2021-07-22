360ee21b591add6ce395d33420a543c11539b72e
kayato <h.kato345@gmail.com>
2021-07-23 03:24:11 +0530

    Updated cart list

--- frontend/src/containers/Cart.jsx
+++ frontend/src/containers/Cart.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import productCameraImage from '../assets/img/product-camera.jpg'
+import React from "react";
+import productCameraImage from "../assets/img/product-camera.jpg";
+import ImgIconTrash from "../assets/img/icon-trash.svg";
 
 //object
 // const cart = [
@@ -16,40 +17,31 @@ import productCameraImage from '../assets/img/product-camera.jpg'
 // ]
 
 const Cart = () => {
-    return (
-        <>
-        <h2>SHOPPING CART</h2>
-        <section className="cart-component">
-            <div className="card">
-                <img className="imageview" 
-                src={productCameraImage}/>
-                <p>Sony PlayStation 4 Pro White Version</p>
-                <p><span>$</span>399.99</p>
-                
-                <div class="dropdown">
-  
-                <select name="cartitems" id="itemnumber">
-                    <option value="1">1</option> 
-                    <option value="2">2</option>      
-                    <option value="3">3</option>       
-                    <option value="4">4</option>
-                </select>
-            </div> |
-            <span><img src="https://img.icons8.com/material-two-tone/24/000000/trash.png"/></span>
-            
+  return (
+    <div class="cart-list">
+      <h2>SHOPPING CART</h2>
+      <div class="content">
+        <section class="product-list">
+          <div class="product">
+            <img src="https://res.cloudinary.com/techis/image/upload/v1626895865/f8tmpqyozntqng0uzt9d.png" alt="" />
+            <div class="product-info">
+              <div class="name">Sony Playstation 4 Pro White Version</div>
+              <div class="bottom">
+                <div class="price">$399.99</div>
+                <div class="quantity-trash">
+                  <div class="quantity">Quantity</div>
+                  <div class="trash">
+                    <img src={ImgIconTrash} alt="" />
+                  </div>
+                </div>
+              </div>
             </div>
-            <hr/>
-            
+          </div>
         </section>
-        <div className="col card mb-3" >
-        <div className="row g-0">
-            <h2>Subtotal</h2>
-            <p><span>(3)</span>ITEMS<span>$</span><p>649.97</p></p>
-            <button>Proceed to checkout</button>
-        </div>
+        <section class="checkout-area"></section>
+      </div>
     </div>
-    </>
-      )
-}
+  );
+};
 
-export default Cart
+export default Cart;

6e3e49d63aa60772d85068976c53f92511a5a67a
Saadi <hardyhar06@gmail.com>
2021-07-22 14:42:26 -0500

    card.jsx

--- frontend/src/components/wishlist/cards.jsx
+++ frontend/src/components/wishlist/cards.jsx
@@ -1,72 +1,163 @@
 import React, {useEffect, useState} from 'react'
 import ImgHomeMainPc from "./img/home-main-pc.jpg"
+import Like from "./img/Like.png"
+import Unlike from "./img/unlike.png"
 
 
 const Cards = () => {
 
-    return (
-        <div className="fav-container">
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
-                </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
-                </div>
-            </div>
+    const [show, setShow] = useState(false);
+    const [likeUnlike, setLikeUnlike] = useState(true);
+    const [editDone, setEditDone] = useState(true);
 
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
-                </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
-                </div>
+    return (
+        <div>
+            <div className="fav-header">
+                    <h1 className="favorites">Favorites</h1>
+                    <button className="edit-button" onClick={() => {setShow(true); setEditDone(false)}}>
+                        {editDone? "Edit":"Done"}
+                    </button>
             </div>
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
+                      
+            <div className="fav-container">
+                
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
                 </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
                 </div>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
+                </div>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
+                </div>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
+                </div>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
+                </div>
+                <div className="Card">
+
+                    {likeUnlike ? 
+                       <>
+                            {
+                                show?<div className="like" onClick={() => setLikeUnlike(false)}><img src={Like} alt="" /></div> :null
+                            }
+                       </>
+                       :<div className="like" onClick={() => setLikeUnlike(false)}><img src={Unlike} alt="" /></div>
+                    }
+                    <div className="card-image">
+                        <img src={ImgHomeMainPc} class="main-pc-img" />
+                    </div>
+                    <div className="card-body">
+                        <h3>Product</h3>
+                        <h5>EOS 80D DSLR</h5>
+                        <h5>Price: $$$</h5>
+                    </div>
+                </div>
+                
             </div>
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
-                </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
-                </div>
-            </div>
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
-                </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
-                </div>
-            </div>
-            <div className="Card">
-                <div className="card-image">
-                    <img src={ImgHomeMainPc} class="main-pc-img" />
-                </div>
-                <div className="card-body">
-                    <h3>Product</h3>
-                    <p>EOS 80D DSLR</p>
-                    <p>Price: $$$</p>
-                </div>
-            </div>
+           
+            
         </div>
             
     )

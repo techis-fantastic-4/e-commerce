27b0ea049dc65a9d3061300c71c0f08a059603a3
Casey Bloom <CaseyBloom@gmail.com>
2021-07-27 13:45:18 -0400

    modified css

--- frontend/src/containers/ProductDetails.jsx
+++ frontend/src/containers/ProductDetails.jsx
@@ -9,7 +9,7 @@ const api = new API();
 const ProductDetails = () => {
     const dispatch = useDispatch();
     const [product, setProduct] = useState(null);
-    
+
     let id = window.location.pathname.split("/products")[1];
     if (id !== "") {
         id = id.split("/")[1];
@@ -17,13 +17,13 @@ const ProductDetails = () => {
 
     useEffect(() => {
         api.getProduct(id).then((product) => {
-          setProduct(product);
-          console.log(product.results[0]);
+            setProduct(product);
+            console.log(product.results[0]);
         });
-      }, []);
+    }, []);
 
-    
-    
+
+
     let status = "";
     if (product && product.results[0].quantity > 0) {
         status = "In Stock";
@@ -36,50 +36,53 @@ const ProductDetails = () => {
             <section className="product">
                 <div className="details-card">
                     <h1 className="details-label">Item Details</h1>
-                    {product && (
-                        <img src={ImgProductCamera} alt="product" class="details-img" />    
-                    )}
-                    
-                        {/* product.results[0].main_image */}
-                    <div class="text-area details-box">
-                    {product && (
-                        <div class="product-price">PRICE : {product.results[0].price}</div>
-                    )}  
-                    {product && (
-                        <div class="product-status">STATUS : {status}</div>
-                    )}    
-                        <div class="form-group" style={{marginBottom: "1rem"}}>
-                            <select class="custom-select">
-                                <option>Qty 1</option>
-                                <option>Qty 2</option>
-                                <option>Qty 3</option>
-                                <option>Qty 4</option>
-                                <option>Qty 5</option>
-                            </select>
+                    <div className="details-container">
+                        <div className="product-image">
+                            {product && (
+                                <img src={product.results[0].main_image} alt="product" class="details-img" />
+                            )}
                         </div>
-                        <div class="cta-area">
-                            <a class="btn btn-primary" id="add-cart-btn" href="   /">
-                                Add to Cart
-                            </a>
+                        
+                        <div className="text-area details-box">
+                            {product && (
+                                <div className="product-price">PRICE : {product.results[0].price}</div>
+                            )}
+                            {product && (
+                                <div className="product-status">STATUS : {status}</div>
+                            )}
+                            <div className="form-group" style={{ marginBottom: "1rem" }}>
+                                <select className="custom-select">
+                                    <option>Qty 1</option>
+                                    <option>Qty 2</option>
+                                    <option>Qty 3</option>
+                                    <option>Qty 4</option>
+                                    <option>Qty 5</option>
+                                </select>
+                            </div>
+                            <div className="cta-area">
+                                <a className="btn btn-primary" id="add-cart-btn" href="   /">
+                                    Add to Cart
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <p class="details-name">
-                    {product && (
-                        <strong>{product.results[0].name}</strong>
-                    )}    
-                        
+                    <p className="details-name">
+                        {product && (
+                            <strong>{product.results[0].name}</strong>
+                        )}
+
                     </p>
                 </div>
                 <hr />
-                    <p class="details-desc">
-                        Item Details:
-                    </p>
-                    <p class="details-desc">
+                <p class="details-desc">
+                    Item Details:
+                </p>
+                <p class="details-desc">
                     {product && (
                         <div>{product.results[0].description}</div>
                     )}
-                        
-                    </p>
+
+                </p>
                 <hr />
 
             </section>

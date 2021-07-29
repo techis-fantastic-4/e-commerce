34ef931cb18d92c9828d56702a04a216c2cab9e3
kayato <h.kato345@gmail.com>
2021-07-30 00:44:04 +0530

    Updated api wishlist

--- backend/apps/wishlists/views.py
+++ backend/apps/wishlists/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, filters
-from .serializers import WishlistSerializer
-from ..users.mixins import CustomLoginRequiredMixin
+from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
+from .serializers import WishlistSerializer, WishlistAddSerializer
+from ..users.mixins import CustomLoginRequiredMixin
+from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Wishlist
 
@@ -16,20 +18,28 @@ class WishList(CustomLoginRequiredMixin, generics.ListAPIView):
         self.queryset = Wishlist.objects.order_by('-created_at').filter(user=request.login_user)
         return self.list(request, *args, **kwargs)
 
+
 class WishlistAdd(CustomLoginRequiredMixin, generics.CreateAPIView):
     queryset = Wishlist.objects.all()
-    serializer_class = WishlistSerializer
+    serializer_class = WishlistAddSerializer
+
+    def post(self, request, *args, **kwargs):
+        # Set the user who login
+        request.data['user'] = request.login_user.id
+        return self.create(request, *args, **kwargs)
 
 
 class WishlistDelete(CustomLoginRequiredMixin, generics.DestroyAPIView):
     queryset = Wishlist.objects.all()
     serializer_class = WishlistSerializer
 
-    
+    def delete(self, request, *args, **kwargs):
+        wishlist = Wishlist.objects.get(pk=self.kwargs['pk'])
+        if wishlist.user.id != request.login_user.id:
+            response = Response({'error': 'You can not delete the wishlist not owned by you.'}, status=status.HTTP_404_NOT_FOUND)
+            response.accepted_renderer = JSONRenderer()
+            response.accepted_media_type = "application/json"
+            response.renderer_context = {}
+            return response
 
-
-# class WishAdd(CustomLoginRequiredMixin, generics.CreateAPIView):
-
-#     def post(self, request, *args, **kwargs):
-#         request.data.post['user_id'] = request.login_user.id
-#         return self.create(request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
